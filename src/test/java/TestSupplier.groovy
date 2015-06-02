6ac8deeccf9d25db5508986ed8c59beecc1de471
Nagase <yoshimasa_nagase@tsyscom.co.jp>
2015-06-02 09:34:55 +0900

    Test case 追加

---
+++ src/test/java/TestSupplier.groovy
@@ -0,0 +1,32 @@
+import spock.lang.Specification;
+
+import com.example.domain.Supplier;
+import com.example.log.demo.Calc;
+
+
+class TestSupplier  extends Specification{
+	private Calc instance;
+	private Supplier supplier;
+	
+		def setup() {
+			instance = new Calc()
+			supplier=new Supplier()
+			
+			
+		}
+	
+		def "２つの値の合計チェック"() {
+			
+			expect:
+			
+	
+			where:
+			a | b | c
+			1 | 3 | 4
+			7 | 4 | 11
+			0 | 0 | 0
+			
+		}
+		
+	
+}
