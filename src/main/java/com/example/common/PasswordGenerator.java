0592aa4a9430e9976c2f99dce4a1020dcbe65f9b
Nagase <yoshimasa_nagase@tsyscom.co.jp>
2015-07-09 15:56:54 +0900

    Check style 適用

--- src/main/java/com/example/common/PasswordGenerator.java
+++ src/main/java/com/example/common/PasswordGenerator.java
@@ -3,21 +3,22 @@ package com.example.common;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordGenerator {
-	public static void main(String[] args){
-		String pass ="demo";
-		
-		BCryptPasswordEncoder bcEncoder =new BCryptPasswordEncoder();
-		String encodedPass =bcEncoder.encode(pass);
-		
-		System.out.println(pass+"=" + encodedPass);
-		System.out.println("result=" + bcEncoder.matches(pass, encodedPass));
-		System.out.println("this is test code(com.example.domain.PasswordGenerator.java:4)");
+  public static void main(String[] args) {
+    String pass = "demo";
 
-		System.out.println("this is Not OK  c.e.c.PasswordGenerator.main(PasswordGenerator.java:19)");
-		System.out.println("this is OK  com.example.common.PasswordGenerator.main(PasswordGenerator.java:19)");
-		
-		bcEncoder=null;
-		bcEncoder.toString();
-		
-	}
+    BCryptPasswordEncoder bcEncoder = new BCryptPasswordEncoder();
+    String encodedPass = bcEncoder.encode(pass);
+
+    System.out.println(pass + "=" + encodedPass);
+    System.out.println("result=" + bcEncoder.matches(pass, encodedPass));
+    System.out.println("this is test code(com.example.domain.PasswordGenerator.java:4)");
+
+    System.out.println("this is Not OK  c.e.c.PasswordGenerator.main(PasswordGenerator.java:19)");
+    System.out
+        .println("this is OK  com.example.common.PasswordGenerator.main(PasswordGenerator.java:19)");
+
+    bcEncoder = null;
+    bcEncoder.toString();
+
+  }
 }
