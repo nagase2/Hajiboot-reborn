35d7c303e35a72b5dcadacaa24001017f7228ef2
Nagase <yoshimasa_nagase@tsyscom.co.jp>
2015-07-29 16:51:38 +0900

    Sonarの設定を変更

--- src/main/java/com/example/domain/Food.java
+++ src/main/java/com/example/domain/Food.java
@@ -1,23 +1,18 @@
 package com.example.domain;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// @Entity
-// @Table(name="customers")//テーブル名（＝クラス名）
-
+//@Entity
+@Table(name="food")//テーブル名（＝クラス名）
 @Data
-@NoArgsConstructor
-// JPAの仕様で、EntityClassには引数のないデフォルトコンストラクタを作る必要がある。
+@NoArgsConstructor // JPAの仕様で、EntityClassには引数のないデフォルトコンストラクタを作る必要がある。
 @AllArgsConstructor
 public class Food {
   @Id
