7ed3e64893b8d1acd65c68f25827bbbe4d510359
Nagase <yoshimasa_nagase@tsyscom.co.jp>
2015-08-03 09:37:34 +0900

    Test系の小変更

---
+++ src/main/java/hello/world/jpa/AAAAA.java
@@ -0,0 +1,13 @@
+package hello.world.jpa;
+
+
+public class AAAAA {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+//    NagaseUtil2 n = new NagaseUtil2();
+//    int result = n.addition(1, 3);
+    // System.out.println("xxxxxxxxxx"+result);
+  }
+
+}
